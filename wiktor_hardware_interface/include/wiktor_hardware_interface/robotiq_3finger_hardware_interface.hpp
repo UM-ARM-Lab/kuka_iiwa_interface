6065e77c87fefe2b8b3126f22e361c68347408a2
Calder Phillips-Grafflin <calder.pg@gmail.com>
2017-03-30 15:20:08 -0400

    Fixed timestamps in messages, starting hardware interface.

--- wiktor_hardware_interface/include/wiktor_hardware_interface/robotiq_3finger_hardware_interface.hpp
+++ wiktor_hardware_interface/include/wiktor_hardware_interface/robotiq_3finger_hardware_interface.hpp
@@ -12,6 +12,8 @@
 // LCM type headers
 #include <wiktor_hardware_interface/robotiq_3finger_command.hpp>
 #include <wiktor_hardware_interface/robotiq_3finger_status.hpp>
+// LCM
+#include <lcm/lcm-cpp.hpp>
 
 #ifndef ROBOTIQ_3FINGER_HARDWARE_INTERFACE_HPP
 #define ROBOTIQ_3FINGER_HARDWARE_INTERFACE_HPP
@@ -20,9 +22,29 @@ namespace robotiq_3finger_hardware_interface
 {
     class Robotiq3FingerHardwareInterface
     {
+    protected:
+
+        std::shared_ptr<lcm::LCM> lcm_ptr_;
+        std::string command_channel_name_;
+        std::string status_channel_name_;
+
+        wiktor_hardware_interface::robotiq_3finger_actuator_command ConvertFingerCommand(const wiktor_hardware_interface::Robotiq3FingerActuatorCommand& finger_command);
+
+        wiktor_hardware_interface::Robotiq3FingerActuatorStatus ConvertFingerStatus(const wiktor_hardware_interface::robotiq_3finger_actuator_status& finger_status) const;
+
+        wiktor_hardware_interface::Robotiq3FingerObjectStatus ConvertObjectStatus(const wiktor_hardware_interface::robotiq_3finger_object_status& object_status) const;
+
+        wiktor_hardware_interface::Robotiq3FingerStatus ConvertStatus(const wiktor_hardware_interface::robotiq_3finger_status& status) const;
+
+        wiktor_hardware_interface::robotiq_3finger_command ConvertCommand(const wiktor_hardware_interface::Robotiq3FingerCommand& command) const;
+
     public:
 
-        Robotiq3FingerHardwareInterface();
+        Robotiq3FingerHardwareInterface(const std::shared_ptr<lcm::LCM>& lcm_ptr, const std::string& command_channel_name, const std::string& status_channel_name);
+
+        bool SendCommandMessage(const wiktor_hardware_interface::Robotiq3FingerCommand& command);
+
+
     };
 }
 #endif // ROBOTIQ_3FINGER_HARDWARE_INTERFACE_HPP
