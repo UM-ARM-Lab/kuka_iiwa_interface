835e184749fc507a0bfdd70d0b22d4e7e50f431d
Dale McConachie <dmcconac@umich.edu>
2018-01-09 15:48:57 -0500

    Updated xbox_control.py to control the scissor. Added initialization loop to manual_motion.py to check for failure to switch modes.

--- victor_hardware_interface/scripts/xbox_control.py
+++ victor_hardware_interface/scripts/xbox_control.py
@@ -20,13 +20,14 @@ class VictorJoystick:
              "left": rospy.Publisher("left_arm/gripper_command", Robotiq3FingerCommand,
                                      queue_size = 1)}
 
-        self.joy_sub = rospy.Subscriber("/joy", Joy, self.joy_callback)
+        self.joy_sub = rospy.Subscriber("joy", Joy, self.joy_callback)
 
     def joy_callback(self, joy):
         A, B, X, Y, LB, RB, back, start, power, stick_button_left, stick_button_right, \
             dleft, dright, dup, ddown = joy.buttons
         left_hor, left_vert, LT, right_hor, right_vert,  RT, d_hor, d_vert = joy.axes
 
+        # Open and close the gripper fingers
         if LT == -1:
             self.close_gripper("left")
 
@@ -39,31 +40,60 @@ class VictorJoystick:
         if RB:
             self.open_gripper("right")
 
+        # Open and close the scissors on the gripper
+        if X:
+            self.close_scissor("left")
+
+        if Y:
+            self.open_scissor("left")
+
+        if A:
+            self.close_scissor("right")
+
+        if B:
+            self.open_scissor("right")
+
     def close_gripper(self, gripper_name, **kwargs):
-        self.set_gripper(gripper_name, (1.0, 1.0, 1.0), **kwargs)
+        self.set_gripper(gripper_name, finger_pos=(1.0, 1.0, 1.0), **kwargs)
 
     def open_gripper(self, gripper_name, **kwargs):
-        self.set_gripper(gripper_name, (0.0, 0.0, 0.0), **kwargs)
+        self.set_gripper(gripper_name, finger_pos=(0.0, 0.0, 0.0), **kwargs)
 
-    def set_gripper(self, gripper_name, pos, blocking=True, continuous=False):
+    def close_scissor(self, gripper_name, **kwargs):
+        self.set_gripper(gripper_name, scissor_pos=1.0, **kwargs)
+
+    def open_scissor(self, gripper_name, **kwargs):
+        self.set_gripper(gripper_name, scissor_pos=0.0, **kwargs)
+
+    def set_gripper(self, gripper_name, finger_pos=None, scissor_pos=None, blocking=True, continuous=False):
         """
-        Sets the gripper finger position. Does not set scissor position
+        Sets the gripper finger position, as well as the scissor
 
         Parameters:
         gripper_name  string  - "left" or "right"
-        pos       float[]   - position values for fingers a,b,c. [0 to 1]
+        finger_pos    float[] - position values for fingers a,b,c. [0 to 1]
+        scissor_pos   float   - position values for scissor. [0 to 1]
         """
         
-        a = pos[0]
-        b = pos[1]
-        c = pos[2]
         cur = self.gripper_status[gripper_name].get()
         cmd = self.default_gripper_command()
-        
-        cmd.finger_a_command.position = a
-        cmd.finger_b_command.position = b
-        cmd.finger_c_command.position = c
-        cmd.scissor_command.position = cur.scissor_status.position_request
+
+        # Set the finger position if commanded
+        if finger_pos is not None:
+            cmd.finger_a_command.position = finger_pos[0]
+            cmd.finger_b_command.position = finger_pos[1]
+            cmd.finger_c_command.position = finger_pos[2]
+        else:
+            cmd.finger_a_command.position = cur.finger_a_status.position_request
+            cmd.finger_b_command.position = cur.finger_b_status.position_request
+            cmd.finger_c_command.position = cur.finger_c_status.position_request
+
+        # Set the scissor position if commanded
+        if scissor_pos is not None:
+            cmd.scissor_command.position = scissor_pos
+        else:
+            cmd.scissor_command.position = cur.scissor_status.position_request
+
         self.gripper_command_publisher[gripper_name].publish(cmd)
 
     def default_gripper_command(self):
