facc600d6f268394818957d34da49241369e010f
Brad Saund <brad.saund@gmail.com>
2018-01-30 14:26:20 -0500

    formatting

--- victor_hardware_interface/SunriseWorkspace/UofMR1_2017_RightUpper/src/armlab/lcm/robotInterface/Conversions.java
+++ victor_hardware_interface/SunriseWorkspace/UofMR1_2017_RightUpper/src/armlab/lcm/robotInterface/Conversions.java
@@ -170,7 +170,7 @@ public class Conversions
     
     public static Transformation cvqToTransformation(final cartesian_value_quantity cvq)
     {
-        return Transformation.ofRad(cvq.x*1000,cvq.y*1000,cvq.z*1000,cvq.a,cvq.b,cvq.c);
+        return Transformation.ofRad(cvq.x*1000, cvq.y*1000, cvq.z*1000, cvq.a, cvq.b,cvq.c);
     }
 
     public static Frame cvqToFrame(final cartesian_value_quantity cvq)
@@ -277,6 +277,7 @@ public class Conversions
     
     public static Frame poseToFrame(final cartesian_pose pose)
     {
+        // The Kuka framework wants translations in mm, so we do the conversion here
         Vector translation = Vector.of(pose.xt * 1000.0, pose.yt * 1000.0, pose.zt * 1000.0);
         MatrixRotation rotMatrix = quatToMatrix(pose.wr, pose.xr, pose.yr, pose.zr);
         return new Frame(Transformation.of(translation, rotMatrix));    
