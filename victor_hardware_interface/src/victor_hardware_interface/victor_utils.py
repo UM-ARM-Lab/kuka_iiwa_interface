99df885537d552b468579f926efa3152c97ef7eb
Peter Mitrano <pmitrano@armstorm>
2020-10-26 09:55:06 -0400

    small cleanup in victor utils
    also remove hard-coded prefix slash in paths

--- victor_hardware_interface/src/victor_hardware_interface/victor_utils.py
+++ victor_hardware_interface/src/victor_hardware_interface/victor_utils.py
@@ -1,15 +1,30 @@
 #! /usr/bin/env python
 
 from enum import Enum
-import rospy
 
+import rospy
 from victor_hardware_interface_msgs.msg import *
 from victor_hardware_interface_msgs.srv import *
+
 
 class Stiffness(Enum):
     STIFF = 1
     MEDIUM = 3
-    SOFT  = 2
+    SOFT = 2
+
+
+def get_new_control_mode(control_mode: ControlMode, stiffness=Stiffness.MEDIUM, vel=0.1, accel=0.1):
+    if control_mode == ControlMode.JOINT_POSITION:
+        return get_joint_position_params(vel, accel)
+    elif control_mode == ControlMode.JOINT_IMPEDANCE:
+        return get_joint_impedance_params(stiffness, vel, accel)
+    elif control_mode == ControlMode.CARTESIAN_IMPEDANCE:
+        return get_cartesian_impedance_params(velocity=vel)
+    elif control_mode == ControlMode.CARTESIAN_POSE:
+        raise NotImplementedError("Cartesian Mode not yet implemented")
+    else:
+        raise NotImplementedError(f"Unknown control mode requested: {control_mode}")
+
 
 def set_control_mode(control_mode, arm, stiffness=Stiffness.MEDIUM, vel=0.1, accel=0.1):
     """
@@ -20,39 +35,19 @@ def set_control_mode(control_mode, arm, stiffness=Stiffness.MEDIUM, vel=0.1, acc
     arm (string):               The name of the arm: "right_arm" or "left_arm"
     stiffness (Stiffness):      For impedance modes, uses a set of stiffness values
     """
-
-
-    new_control_mode = ControlModeParameters()
-    new_control_mode.control_mode.mode = control_mode
-
-    if control_mode == ControlMode.JOINT_POSITION:
-        new_control_mode = get_joint_position_params(vel=vel, accel=accel)
-
-    elif control_mode == ControlMode.JOINT_IMPEDANCE:
-        new_control_mode = get_joint_impedance_params(stiffness, vel=vel, accel=accel)
-
-    elif control_mode == ControlMode.CARTESIAN_POSE:
-        rospy.logerr("Cartesian Mode not yet implemented")
-        assert(False)
-
-    elif control_mode == ControlMode.CARTESIAN_IMPEDANCE:
-        rospy.logerr("Cartesian Mode not yet implemented")
-        assert(False)
-
-    else:
-        rospy.logerr("Unknown control mode requested: " + str(control_mode))
-        assert(False)
-
+    new_control_mode = get_new_control_mode(control_mode, stiffness, vel, accel)
     result = send_new_control_mode(arm, new_control_mode)
     if not result.success:
         rospy.logerr("Failed to switch to control mode: " + str(control_mode))
     return result
 
+
 def send_new_control_mode(arm, msg):
     # TODO: Consider removing the forced global namespace in the future
-    send_new_control_mode_srv = rospy.ServiceProxy("/" + arm + "/set_control_mode_service",
+    send_new_control_mode_srv = rospy.ServiceProxy(arm + "/set_control_mode_service",
                                                    SetControlMode)
     return send_new_control_mode_srv(msg)
+
 
 def get_joint_position_params(vel, accel):
     new_control_mode = ControlModeParameters()
@@ -60,6 +55,7 @@ def get_joint_position_params(vel, accel):
     new_control_mode.joint_path_execution_params.joint_relative_velocity = vel
     new_control_mode.joint_path_execution_params.joint_relative_acceleration = accel
     return new_control_mode
+
 
 def get_joint_impedance_params(stiffness, vel=0.1, accel=0.1):
     """
@@ -127,10 +123,66 @@ def get_joint_impedance_params(stiffness, vel=0.1, accel=0.1):
 
     else:
         rospy.logerr("Unknown stiffness for Joint Impedance")
-        assert(False)
+        assert False
 
     return new_control_mode
 
+
+def get_cartesian_impedance_params(velocity=2.5):
+    """
+    Returns predefined cartesian impedance parameters
+    """
+    # TODO allow sub-modes such as xy planar motion
+    new_control_mode = ControlModeParameters()
+    new_control_mode.control_mode.mode = ControlMode.CARTESIAN_IMPEDANCE
+    new_control_mode.cartesian_path_execution_params.max_velocity.x = velocity
+    new_control_mode.cartesian_path_execution_params.max_velocity.y = velocity
+    new_control_mode.cartesian_path_execution_params.max_velocity.z = velocity
+    new_control_mode.cartesian_path_execution_params.max_velocity.a = velocity * 0.25
+    new_control_mode.cartesian_path_execution_params.max_velocity.b = velocity * 0.25
+    new_control_mode.cartesian_path_execution_params.max_velocity.c = velocity * 0.25
+    new_control_mode.cartesian_path_execution_params.max_nullspace_velocity = 750.0
+    new_control_mode.cartesian_path_execution_params.max_acceleration.x = 100.0
+    new_control_mode.cartesian_path_execution_params.max_acceleration.y = 100.0
+    new_control_mode.cartesian_path_execution_params.max_acceleration.z = 100.0
+    new_control_mode.cartesian_path_execution_params.max_acceleration.a = 100.0
+    new_control_mode.cartesian_path_execution_params.max_acceleration.b = 100.0
+    new_control_mode.cartesian_path_execution_params.max_acceleration.c = 100.0
+    new_control_mode.cartesian_path_execution_params.max_nullspace_acceleration = 100.0
+    new_control_mode.cartesian_impedance_params.cartesian_damping.x = 0.25
+    new_control_mode.cartesian_impedance_params.cartesian_damping.y = 0.25
+    new_control_mode.cartesian_impedance_params.cartesian_damping.z = 0.25
+    new_control_mode.cartesian_impedance_params.cartesian_damping.a = 0.25
+    new_control_mode.cartesian_impedance_params.cartesian_damping.b = 0.25
+    new_control_mode.cartesian_impedance_params.cartesian_damping.c = 0.25
+    new_control_mode.cartesian_impedance_params.nullspace_damping = 0.5
+    new_control_mode.cartesian_impedance_params.cartesian_stiffness.x = 5000.0
+    new_control_mode.cartesian_impedance_params.cartesian_stiffness.y = 5000.0
+    new_control_mode.cartesian_impedance_params.cartesian_stiffness.z = 5000.0
+    new_control_mode.cartesian_impedance_params.cartesian_stiffness.a = 300.0
+    new_control_mode.cartesian_impedance_params.cartesian_stiffness.b = 300.0
+    new_control_mode.cartesian_impedance_params.cartesian_stiffness.c = 300.0
+    new_control_mode.cartesian_impedance_params.nullspace_stiffness = 100.0
+    new_control_mode.cartesian_control_mode_limits.max_path_deviation.x = 10000000.0
+    new_control_mode.cartesian_control_mode_limits.max_path_deviation.y = 10000000.0
+    new_control_mode.cartesian_control_mode_limits.max_path_deviation.z = 10000000.0
+    new_control_mode.cartesian_control_mode_limits.max_path_deviation.a = 10000000.0
+    new_control_mode.cartesian_control_mode_limits.max_path_deviation.b = 10000000.0
+    new_control_mode.cartesian_control_mode_limits.max_path_deviation.c = 10000000.0
+    new_control_mode.cartesian_control_mode_limits.max_cartesian_velocity.x = velocity
+    new_control_mode.cartesian_control_mode_limits.max_cartesian_velocity.y = velocity
+    new_control_mode.cartesian_control_mode_limits.max_cartesian_velocity.z = velocity
+    new_control_mode.cartesian_control_mode_limits.max_cartesian_velocity.a = velocity * 2.0 * 0.25
+    new_control_mode.cartesian_control_mode_limits.max_cartesian_velocity.b = velocity * 2.0 * 0.25
+    new_control_mode.cartesian_control_mode_limits.max_cartesian_velocity.c = velocity * 2.0 * 0.25
+    new_control_mode.cartesian_control_mode_limits.max_control_force.x = 20.0
+    new_control_mode.cartesian_control_mode_limits.max_control_force.y = 20.0
+    new_control_mode.cartesian_control_mode_limits.max_control_force.z = 20.0
+    new_control_mode.cartesian_control_mode_limits.max_control_force.a = 20.0
+    new_control_mode.cartesian_control_mode_limits.max_control_force.b = 20.0
+    new_control_mode.cartesian_control_mode_limits.max_control_force.c = 20.0
+    new_control_mode.cartesian_control_mode_limits.stop_on_max_control_force = False
+    return new_control_mode
 
 
 def jvq_to_list(jvq):
@@ -141,7 +193,8 @@ def jvq_to_list(jvq):
     Return:
     list with 7 elements
     """
-    return [getattr(jvq, 'joint_' + str(num)) for num in range(1,8)]
+    return [getattr(jvq, 'joint_' + str(num)) for num in range(1, 8)]
+
 
 def list_to_jvq(quantity_list):
     """Turns a list of 7 numbers into a joint value quantity
@@ -151,11 +204,12 @@ def list_to_jvq(quantity_list):
     Return:
     jvq JointValueQuantity
     """
-    assert(len(quantity_list) == 7)
+    assert (len(quantity_list) == 7)
     jvq = JointValueQuantity()
     for i in range(7):
-        setattr(jvq, 'joint_' + str(i+1), quantity_list[i])
+        setattr(jvq, 'joint_' + str(i + 1), quantity_list[i])
     return jvq
+
 
 def default_gripper_command():
     cmd = Robotiq3FingerCommand()
