2b2ead500a760857627a7a426565d29e1462be48
Peter Mitrano <pmitrano@armstorm>
2020-11-01 15:06:59 -0500

    finish refactoring vfhi

--- victor_fake_hardware_interface/scripts/fake_grippers_node.py
+++ victor_fake_hardware_interface/scripts/fake_grippers_node.py
@@ -15,13 +15,13 @@ def main():
                                                                    gripper_status_topic=arm + "/gripper_status")
 
     for arm in arm_names:
-        interfaces[arm].start_feedback_threads()
+        interfaces[arm + "/gripper"].start_feedback_threads()
 
     rospy.loginfo("Publishing data...")
     rospy.spin()
 
     for arm in arm_names:
-        interfaces[arm].join_feedback_threads()
+        interfaces[arm + "/gripper"].join_feedback_threads()
 
 
 if __name__ == '__main__':
