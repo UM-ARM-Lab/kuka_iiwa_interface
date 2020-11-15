fe645792683eac406a893c7d1630c5d541f0db79
Brad Saund <brad.saund@gmail.com>
2020-11-15 11:45:43 -0500

    Update Naming to be consistent with new URDF and namespaces

--- victor_fake_hardware_interface/src/victor_fake_hardware_interface/minimal_fake_arm_interface.py
+++ victor_fake_hardware_interface/src/victor_fake_hardware_interface/minimal_fake_arm_interface.py
@@ -277,32 +277,3 @@ class MinimalFakeArmInterface:
         return msg
 
 
-def main():
-    rospy.init_node("minimal_fake_arm_interface")
-
-    interfaces = {}
-
-    arm_names = ["left_arm", "right_arm"]
-
-    for arm in arm_names:
-        interfaces[arm] = MinimalFakeArmInterface(arm_name=arm,
-                                                  control_mode_status_topic=arm + "/control_mode_status",
-                                                  get_control_mode_service_topic=arm + "/get_control_mode_service",
-                                                  set_control_mode_service_topic=arm + "/set_control_mode_service",
-                                                  motion_command_topic=arm + "/motion_command",
-                                                  motion_status_topic=arm + "/motion_status")
-        interfaces[arm + "/gripper"] = MinimalFakeGripperInterface(gripper_command_topic=arm + "/gripper_command",
-                                                                   gripper_status_topic=arm + "/gripper_status")
-
-    for arm in arm_names:
-        interfaces[arm].start_feedback_threads()
-
-    rospy.loginfo("Publishing data...")
-    rospy.spin()
-
-    for arm in arm_names:
-        interfaces[arm].join_feedback_threads()
-
-
-if __name__ == "__main__":
-    main()
