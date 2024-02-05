0f86cf63390c1aefe469aa74973f61f494ea544b
Peter <peter@thinkpad_t15>
2024-02-05 17:44:18 -0500

    testing LCM from python

--- victor_hardware/lcm_defs/make_defs.sh
+++ victor_hardware/lcm_defs/make_defs.sh
@@ -12,3 +12,5 @@ rm -r ../lcm_types/victor_lcm_interface
 
 echo "Moving generated types"
 mv ./victor_lcm_interface ../lcm_types/
+echo "Moving python packages to match ament python package structure"
+mv ../lcm_types/victor_lcm_interface/*.py ../victor_hardware/victor_lcm_interface/
