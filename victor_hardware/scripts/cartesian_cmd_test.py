343b4d30962cf49badb2907ff59c805ca841d70f
Peter <pmitrano@umich.edu>
2024-02-19 15:50:53 -0500

    setup cmd test

--- victor_hardware/scripts/cartesian_cmd_test.py
+++ victor_hardware/scripts/cartesian_cmd_test.py
@@ -10,16 +10,15 @@ The motion will be relative in gripper frame to the pose of the gripper when you
 see the vr_ros2_bridge repo for setup instructions.
 """
 
-import time
-from pathlib import Path
+import tf2_ros
+from tf2_py import TransformException
 
-import numpy as np
+from victor_hardware.victor import Victor
+from victor_hardware_interfaces.msg import MotionCommand, ControlMode
 
 import rclpy
-from victor_hardware.victor import Victor
-from geometry_msgs.msg import Pose
-from geometry_msgs.msg import TransformStamped
 from rclpy.node import Node
+
 
 class CartesianCommandTestNode(Node):
 
@@ -28,12 +27,41 @@ class CartesianCommandTestNode(Node):
 
         self.victor = Victor(self)
 
-        # subscribe to TF
+        # create a TF listener
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
-        # setup ros timer
+        # Set up ros timer
+        self.timer = self.create_timer(0.1, self.timer_cb)
+
+        self.victor.set_left_arm_control_mode(ControlMode.CARTESIAN_IMPEDANCE)
+
+        self.t0 = None
 
     def timer_cb(self):
-        pass
+        if self.t0 is None:
+            # try to get the current pose of the left gripper
+            try:
+                self.t0 = self.tf_buffer.lookup_transform('victor_left_palm', 'victor_left_arm_link0', rclpy.time.Time())
+                print(f"Got transform: {self.t0}")
+            except TransformException as ex:
+                self.get_logger().info(f'Could not transform: {ex}')
+                return
+        else:
+            msg = MotionCommand()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.control_mode.mode = ControlMode.CARTESIAN_IMPEDANCE
+
+            msg.cartesian_pose.position.x = self.t0.transform.translation.x
+            msg.cartesian_pose.position.y = self.t0.transform.translation.y
+            msg.cartesian_pose.position.z = self.t0.transform.translation.z
+            msg.cartesian_pose.orientation.x = self.t0.transform.rotation.x
+            msg.cartesian_pose.orientation.y = self.t0.transform.rotation.y
+            msg.cartesian_pose.orientation.z = self.t0.transform.rotation.z
+            msg.cartesian_pose.orientation.w = self.t0.transform.rotation.w
+
+            self.victor.left_arm_cmd_pub.publish(msg)
+
 
 def main():
     rclpy.init()
