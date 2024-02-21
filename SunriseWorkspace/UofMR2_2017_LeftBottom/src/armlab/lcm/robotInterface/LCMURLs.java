f81c1c84975daa3313bff3799a9a664f3c18f90d
armlab <armlab@armistice>
2024-02-21 12:54:38 -0500

    use Loki IP address instead of kukacontrol

--- SunriseWorkspace/UofMR2_2017_LeftBottom/src/armlab/lcm/robotInterface/LCMURLs.java
+++ SunriseWorkspace/UofMR2_2017_LeftBottom/src/armlab/lcm/robotInterface/LCMURLs.java
@@ -3,6 +3,6 @@ package armlab.lcm.robotInterface;
 public class LCMURLs
 {
     // Left/Lower arm
-    static final public String DEFAULT_DEST_URL = "udp://10.10.10.108:30002";
+    static final public String DEFAULT_DEST_URL = "udp://10.10.10.169:30002"; // Loki
     static final public String DEFAULT_SELF_URL = "udp://10.10.10.12:30000";
 }
