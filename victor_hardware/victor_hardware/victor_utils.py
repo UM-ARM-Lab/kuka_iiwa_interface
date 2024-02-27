e6392bdaf64681949a7b6e03412a9a05972311d4
armlab <armlab@loki>
2024-02-27 17:42:34 -0500

    tweak SOFT params

--- victor_hardware/victor_hardware/victor_utils.py
+++ victor_hardware/victor_hardware/victor_utils.py
@@ -91,13 +91,13 @@ def get_joint_impedance_params(stiffness, vel=0.1, accel=0.1):
         new_control_mode.joint_impedance_params.joint_damping.joint_5 = 0.7
         new_control_mode.joint_impedance_params.joint_damping.joint_6 = 0.7
         new_control_mode.joint_impedance_params.joint_damping.joint_7 = 0.7
-        new_control_mode.joint_impedance_params.joint_stiffness.joint_1 = 30.0
-        new_control_mode.joint_impedance_params.joint_stiffness.joint_2 = 30.0
-        new_control_mode.joint_impedance_params.joint_stiffness.joint_3 = 15.0
-        new_control_mode.joint_impedance_params.joint_stiffness.joint_4 = 15.0
-        new_control_mode.joint_impedance_params.joint_stiffness.joint_5 = 9.0
-        new_control_mode.joint_impedance_params.joint_stiffness.joint_6 = 9.0
-        new_control_mode.joint_impedance_params.joint_stiffness.joint_7 = 3.0
+        new_control_mode.joint_impedance_params.joint_stiffness.joint_1 = 125.0
+        new_control_mode.joint_impedance_params.joint_stiffness.joint_2 = 125.0
+        new_control_mode.joint_impedance_params.joint_stiffness.joint_3 = 50.0
+        new_control_mode.joint_impedance_params.joint_stiffness.joint_4 = 50.0
+        new_control_mode.joint_impedance_params.joint_stiffness.joint_5 = 25.0
+        new_control_mode.joint_impedance_params.joint_stiffness.joint_6 = 25.0
+        new_control_mode.joint_impedance_params.joint_stiffness.joint_7 = 10.0
 
     else:
         print("Unknown stiffness for Joint Impedance")
