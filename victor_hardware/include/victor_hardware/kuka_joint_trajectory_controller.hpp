6adda8dac5385dc2e6d321b7a538b835931d69f7
armlab <armlab@loki>
2024-03-22 11:53:56 -0400

    testing new override

--- victor_hardware/include/victor_hardware/kuka_joint_trajectory_controller.hpp
+++ victor_hardware/include/victor_hardware/kuka_joint_trajectory_controller.hpp
@@ -2,6 +2,9 @@
 
 #include <controller_interface/controller_interface.hpp>
 #include <joint_trajectory_controller/joint_trajectory_controller.hpp>
+#include <victor_lcm_interface/control_mode_parameters.hpp>
+
+constexpr auto const DEFAULT_RELATIVE_VELOCITY= 0.1;
 
 namespace victor_hardware {
 
@@ -12,7 +15,11 @@ class KukaJointPositionTrajectoryController : public joint_trajectory_controller
   controller_interface::CallbackReturn on_init() override;
 
  private:
+  void sendParamsLCM();
+
   std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
-  std::shared_ptr<rclcpp::ParameterCallbackHandle> cb_handle_;
+  std::vector<std::shared_ptr<rclcpp::ParameterCallbackHandle>> handles_;
+
+  victor_lcm_interface::control_mode_parameters kuka_mode_params_;
 };
 }  // namespace victor_hardware
