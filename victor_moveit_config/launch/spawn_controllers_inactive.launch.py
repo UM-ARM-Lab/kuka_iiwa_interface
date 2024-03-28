77b61d5864bab2830d5569bec4c1e89a40a13457
armlab <armlab@loki>
2024-03-28 15:37:42 -0400

    add inactive control mode launch

---
+++ victor_moveit_config/launch/spawn_controllers_inactive.launch.py
@@ -0,0 +1,21 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("victor", package_name="victor_moveit_config").to_moveit_configs()
+    controller_names = moveit_config.trajectory_execution.get("moveit_simple_controller_manager", {}).get("inactive_controller_names", [])
+    ld = LaunchDescription()
+
+    for controller in controller_names:
+        ld.add_action(
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["--inactive", controller],
+                output="screen",
+            )
+        )
+
+    return ld
