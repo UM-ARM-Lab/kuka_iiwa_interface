4f6360a0d05036e6e402033a7356f983fbcb63c0
yating <yatinlin@umich.edu>
2025-05-06 20:52:41 -0400

    yating's change

--- victor_python/victor_python/victor_left.py
+++ victor_python/victor_python/victor_left.py
@@ -69,6 +69,7 @@ from moveit_msgs.msg import (
     PlanningScene,
     PositionConstraint,
 )
+from victor_python.victor import Side
 
 # Suppress error messages from urdf_parser_py
 core.on_error = lambda *args: None
@@ -88,265 +89,8 @@ class ControlModeError(Exception):
     pass
 
 
-class Side:
 
-    def __init__(self, node: Node, name: str, tf_wrapper: TF2Wrapper):
-        """
-        Args:
-            node: rclpy node
-            name: either "left" or "right"
-        """
-        self.node = node
-        self.tf_wrapper = tf_wrapper
-        self.name = name
-        self.arm_name = f"{self.name}_arm"
-        self.cartesian_cmd_tool_frame = f'victor_{self.arm_name}_{CARTESIAN_CMD_FRAME_SUFFIX}'
-        self.cartesian_cmd_base_frame = f'victor_{self.arm_name}_cartesian_cmd'
-        self.tool_frame = "victor_" + name + "_" + TOOL_FRAME_SUFFIX
-
-        # This depends on moveit being installed and importable, but not on moveit_py or on the move_group node
-        self.moveit_config = load_moveit_config("victor").to_dict()
-        self.urdf = URDF.from_xml_string(self.moveit_config['robot_description'])
-        self.lower, self.upper = self.parser_joint_limits()
-
-        self.pub_group = MutuallyExclusiveCallbackGroup()
-        self.cm_srv_group = MutuallyExclusiveCallbackGroup()
-
-        self.cartesian_cmd_pub = node.create_publisher(Pose, f"{self.arm_name}_cartesian_controller", 10,
-                                                       callback_group=self.pub_group)
-        self.gripper_command = node.create_publisher(Robotiq3FingerCommand, f"victor/{self.arm_name}/gripper_command",
-                                                     10, callback_group=self.pub_group)
-        self.list_controllers_client = node.create_client(ListControllers, f"controller_manager/list_controllers",
-                                                          callback_group=self.cm_srv_group)
-        while not self.list_controllers_client.wait_for_service(timeout_sec=1.0):
-            self.node.get_logger().info('list controller service not available, waiting again...')
-        self.get_get_parameters_client = {}
-        self.motion_status = Listener(node, MotionStatus, f"/victor/{self.arm_name}/motion_status", 10)
-        self.gripper_status = Listener(node, Robotiq3FingerStatus, f"victor/{self.arm_name}/gripper_status", 10)
-        self.control_mode_listener = Listener(node, ControlModeParameters,
-                                              f"victor/{self.arm_name}/control_mode_parameters", 10)
-
-        self.controller_publishers: dict[str, Publisher] = {}
-        self.active_controller_name = None
-
-    def open_gripper(self, scissor_position=0.5):
-        # TODO: implementing blocking grasping
-        self.gripper_command.publish(get_gripper_closed_fraction_msg(ROBOTIQ_OPEN, scissor_position))
-
-    def close_gripper(self, scissor_position=0.5):
-        # TODO: implementing blocking grasping
-        self.gripper_command.publish(get_gripper_closed_fraction_msg(ROBOTIQ_CLOSED, scissor_position))
-
-    def get_gripper_status(self) -> Robotiq3FingerStatus:
-        return self.gripper_status.get()
-
-    def is_gripper_closed(self):
-        status: Robotiq3FingerStatus = self.get_gripper_status()
-        return is_gripper_closed(status)
-
-    def get_motion_status(self) -> MotionStatus:
-        return self.motion_status.get()
-
-    def get_arm_control_mode(self):
-        control_mode_res: ControlModeParameters = self.control_mode_listener.get()
-        return control_mode_res.control_mode
-
-    def get_names_and_cmd_joints(self):
-        status: MotionStatus = self.motion_status.get()
-        commanded_positions = jvq_to_list(status.commanded_joint_position)
-        names = [f"victor_{self.arm_name}_joint_{i}" for i in range(1, 8)]
-        return names, commanded_positions
-
-    def get_estimated_external_force(self):
-        status: MotionStatus = self.motion_status.get()
-        wr = status.estimated_external_wrench
-        T_base_to_palm = self.tf_wrapper.get_transform(
-            "victor_root",
-            f"victor_{self.arm_name}_sunrise_palm_surface")
-        force = np.array([wr.x, wr.y, wr.z, 1])
-        mat_base_to_palm = build_mat_from_transform(T_base_to_palm)
-        mat_base_to_palm[:, 3] = 0
-        force_T = mat_base_to_palm @ force
-        return force_T[:3]
-
-    def get_names_and_measured_joints(self):
-        status: MotionStatus = self.motion_status.get()
-        measured_positions = jvq_to_list(status.measured_joint_position)
-        names = [f"victor_{self.arm_name}_joint_{i}" for i in range(1, 8)]
-        return names, measured_positions
-
-    def get_jtc_cmd_pub(self, active_controller_name: str):
-        if active_controller_name not in self.controller_publishers:
-            jtc_cmd_pub = self.node.create_publisher(JointTrajectory,
-                                                     f'{active_controller_name}/joint_trajectory',
-                                                     10, callback_group=ReentrantCallbackGroup())
-            wait_for_subscriber(jtc_cmd_pub)
-            self.controller_publishers[active_controller_name] = jtc_cmd_pub
-
-        return self.controller_publishers[active_controller_name]
-
-    def get_joint_cmd_pub(self, active_controller_name: str):
-        if active_controller_name not in self.controller_publishers:
-            joint_cmd_pub = self.node.create_publisher(Float64MultiArray, f'/{active_controller_name}/commands', 10)
-            wait_for_subscriber(joint_cmd_pub)
-            self.controller_publishers[active_controller_name] = joint_cmd_pub
-
-        return self.controller_publishers[active_controller_name]
-
-    def send_joint_cmd(self, joint_positions):
-        """
-        Send a Float64MultiArray with joint_positions, ordered joint1 to joint 7.
-        """
-        if len(joint_positions) != 7:
-            raise ValueError(f"joint_positions must be length 7, got {len(joint_positions)}")
-
-        # Check against joint limits
-        if np.any(joint_positions < self.lower) or np.any(joint_positions > self.upper):
-            raise ValueError(f"Joint positions out of bounds: {joint_positions}")
-
-        active_mode: ControlModeParameters = self.control_mode_listener.get()
-        if active_mode.control_mode.mode not in [ControlMode.JOINT_POSITION, ControlMode.JOINT_IMPEDANCE]:
-            raise ControlModeError(f"Cannot send joint command in {active_mode} mode")
-        if self.active_controller_name is None:
-            self.get_and_update_active_controllers()
-        joint_cmd_pub = self.get_joint_cmd_pub(self.active_controller_name)
-        msg = Float64MultiArray()
-        msg.data = list(joint_positions)
-        joint_cmd_pub.publish(msg)
-
-    def send_jtc_cmd(self, joint_positions):
-        pass
-
-    def parser_joint_limits(self):
-        lower = []
-        upper = []
-        for i in range(7):
-            for joint in self.urdf.joints:
-                if joint.name == f"victor_{self.arm_name}_joint_{i + 1}":
-                    lower.append(joint.limit.lower)
-                    upper.append(joint.limit.upper)
-        return np.array(lower), np.array(upper)
-
-    def send_cartesian_cmd(self, target_hand_in_root: TransformStamped):
-        """
-        Fills out, validates, and sends a Pose to the cartesian impedance controller.
-        """
-        active_mode: ControlMode = self.control_mode_listener.get().control_mode
-        if active_mode.mode != ControlMode.CARTESIAN_IMPEDANCE:
-            raise ControlModeError(f"Cannot send cartesian command in {active_mode} mode")
-
-        if target_hand_in_root.header.frame_id != self.cartesian_cmd_base_frame:
-            raise ValueError(f"header.frame_id must be {self.cartesian_cmd_base_frame}")
-        if target_hand_in_root.child_frame_id != self.cartesian_cmd_tool_frame:
-            raise ValueError(f"child_frame_id must be {self.cartesian_cmd_tool_frame}")
-
-        msg = Pose()
-        msg.position.x = target_hand_in_root.transform.translation.x
-        msg.position.y = target_hand_in_root.transform.translation.y
-        msg.position.z = target_hand_in_root.transform.translation.z
-        msg.orientation.w = target_hand_in_root.transform.rotation.w
-        msg.orientation.x = target_hand_in_root.transform.rotation.x
-        msg.orientation.y = target_hand_in_root.transform.rotation.y
-        msg.orientation.z = target_hand_in_root.transform.rotation.z
-
-        self.cartesian_cmd_pub.publish(msg)
-
-    def get_measured_joint_states_from_status(self, include_gripper: bool = True):
-        """
-        Modified the message in-place
-
-        Args:
-            include_gripper: whether to include the gripper in the message
-        """
-        joint_state = JointState()
-        motion_status: MotionStatus = self.get_motion_status()
-        joint_state.position.extend(jvq_to_list(motion_status.measured_joint_position))
-        joint_state.velocity.extend(jvq_to_list(motion_status.measured_joint_velocity))
-        joint_state.effort.extend(jvq_to_list(motion_status.measured_joint_torque))
-        joint_state.name.extend([f"victor_{self.arm_name}_joint_{i}" for i in range(1, 8)])
-
-        if not include_gripper:
-            return joint_state
-
-        gripper_status: Robotiq3FingerStatus = self.get_gripper_status()
-
-        joint_state.position.extend(compute_finger_angles(gripper_status.finger_a_status.position))
-        joint_state.name.extend(get_finger_angle_names(self.name, "finger_a"))
-
-        joint_state.position.append(-compute_scissor_angle(gripper_status.scissor_status.position))
-        joint_state.name.append(get_scissor_joint_name(self.name, "finger_b"))
-
-        joint_state.position.extend(compute_finger_angles(gripper_status.finger_b_status.position))
-        joint_state.name.extend(get_finger_angle_names(self.name, "finger_b"))
-
-        joint_state.position.append(compute_scissor_angle(gripper_status.scissor_status.position))
-        joint_state.name.append(get_scissor_joint_name(self.name, "finger_c"))
-
-        joint_state.position.extend(compute_finger_angles(gripper_status.finger_c_status.position))
-        joint_state.name.extend(get_finger_angle_names(self.name, "finger_c"))
-
-        return joint_state
-
-    def get_active_controller_names(self):
-        controllers = self.get_and_update_active_controllers()
-        controller_names = [controller.name for controller in controllers]
-        return controller_names
-
-    def get_and_update_active_controllers(self) -> List[ControllerState]:
-        controllers = self.get_all_controllers()
-        # remove inactive controllers
-        controllers = [controller for controller in controllers if controller.state == "active"]
-
-        # filter out any control that does not contain any interfaces for this side
-        controllers = [controller for controller in controllers if self.is_claimed_by(controller)]
-        if len(controllers) > 0:
-            self.active_controller_name = controllers[0].name
-        else:
-            self.active_controller_name = None
-        return controllers
-
-    def get_all_controllers(self, mode="async") -> List[ControllerState]:
-        # Call the list controllers ROS service
-        req = ListControllers.Request()
-        if mode == "sync":
-            res = self.list_controllers_client.call(req)
-        elif mode == "async":
-            future = self.list_controllers_client.call_async(req)
-            rate = self.node.create_rate(1000)
-            while not future.done():
-                rate.sleep()
-            res = future.result()
-        controllers = res.controller
-        controllers = [controller for controller in controllers if "broadcaster" not in controller.name]
-        return controllers
-
-    def is_claimed_by(self, controller: ControllerState):
-        return any([self.name in interface for interface in controller.claimed_interfaces])
-
-    def get_control_mode_for_controller(self, controller_name: str) -> str:
-        # get the ROS param "control_mode" on the given controller's node
-        if controller_name not in self.get_get_parameters_client:
-            srv_client = self.node.create_client(GetParameters, f"{controller_name}/get_parameters")
-            success = srv_client.wait_for_service(timeout_sec=1.0)
-            if not success:
-                raise RuntimeError(f"Could not get parameters from {controller_name}")
-            self.get_get_parameters_client[controller_name] = srv_client
-        else:
-            srv_client = self.get_get_parameters_client[controller_name]
-
-        req = GetParameters.Request()
-        req.names = ["control_mode"]
-        future = srv_client.call_async(req)
-        rate = self.node.create_rate(100)
-        while not future.done():
-            rate.sleep()
-        res = future.result()
-        control_mode = res.values[0].string_value
-
-        return control_mode
-
-
-class Victor:
+class VictorLeft:
 
     def __init__(self, node: Node, robot_description_cb: Optional[Callable[[RobotURDF], None]] = None,
                  enable_moveit=True,
@@ -358,7 +102,7 @@ class Victor:
 
         self.tf_wrapper = TF2Wrapper(node)
         self.left = Side(node, 'left', self.tf_wrapper)
-        self.right = Side(node, 'right', self.tf_wrapper)
+        # self.right = Side(node, 'right', self.tf_wrapper)
         self.base_link = "victor_root"
         self.end_effector_name = end_effector_name
         self.move_group_name = move_group_name
@@ -476,12 +220,20 @@ class Victor:
         return self.tf_wrapper.get_transform(base_frame, link_name)
 
     def set_left_controller(self, control_mode: str):
+        """impedance_controller"""
         assert control_mode in ["position_controller", "impedance_controller",
                                 "joint_position_trajectory_controller", "joint_impedance_trajectory_controller",
                                 "cartesian_controller",
                                 ]
         left_active_controllers = self.left.get_active_controller_names()
         active_controllers = list(set(left_active_controllers))
+        # print(f"Active controllers: {active_controllers}")
+        # print(f"{control_mode in active_controllers[0]}")
+        if len(active_controllers) > 0:
+            if control_mode in active_controllers[0] :
+                print(f"Controller {control_mode} is already active")
+                return True
+
         req = SwitchController.Request()
         req.deactivate_controllers = [controller for controller in active_controllers if controller not in control_mode]
         if control_mode in ["joint_position_trajectory_controller", "joint_impedance_trajectory_controller"]:
@@ -533,8 +285,9 @@ class Victor:
 
     def get_and_update_active_controller_names(self):
         left_controller_name = self.left.get_active_controller_names()
-        right_controller_name = self.right.get_active_controller_names()
-        controller_names = {"left": left_controller_name, "right": right_controller_name}
+        # right_controller_name = self.right.get_active_controller_names()
+        # controller_names = {"left": left_controller_name, "right": right_controller_name}
+        controller_names = {"left": left_controller_name}
         return controller_names
 
     def get_moveit_planning_component(self, group_name):
@@ -1086,16 +839,16 @@ class Victor:
         joint_state = JointState()
 
         left_joint_state = self.left.get_measured_joint_states_from_status(include_gripper)
-        right_joint_state = self.right.get_measured_joint_states_from_status(include_gripper)
+        # right_joint_state = self.right.get_measured_joint_states_from_status(include_gripper)
 
         joint_state.position.extend(left_joint_state.position)
-        joint_state.position.extend(right_joint_state.position)
+        # joint_state.position.extend(right_joint_state.position)
         joint_state.velocity.extend(left_joint_state.velocity)
-        joint_state.velocity.extend(right_joint_state.velocity)
+        # joint_state.velocity.extend(right_joint_state.velocity)
         joint_state.effort.extend(left_joint_state.effort)
-        joint_state.effort.extend(right_joint_state.effort)
+        # joint_state.effort.extend(right_joint_state.effort)
         joint_state.name.extend(left_joint_state.name)
-        joint_state.name.extend(right_joint_state.name)
+        # joint_state.name.extend(right_joint_state.name)
 
         # sort based on URDF order
         indices = [joint_state.name.index(name) for name in self.joint_names_urdf_order]
@@ -1107,10 +860,12 @@ class Victor:
         return joint_state
 
     def get_motion_statuses(self):
-        return {'left': self.left.get_motion_status(), 'right': self.right.get_motion_status()}
+        # return {'left': self.left.get_motion_status(), 'right': self.right.get_motion_status()}
+        return {'left': self.left.get_motion_status()}
 
     def get_control_modes(self):
-        return {'left': self.left.get_arm_control_mode(), 'right': self.right.get_arm_control_mode()}
+        # return {'left': self.left.get_arm_control_mode(), 'right': self.right.get_arm_control_mode()}
+        return {'left': self.left.get_arm_control_mode()}
 
     def get_joint_states(self) -> JointState:
         return self.joint_states_listener.get()
@@ -1136,14 +891,16 @@ class Victor:
 
     def get_joint_cmd_dict(self):
         left_names, left_commanded_positions = self.left.get_names_and_cmd_joints()
-        right_names, right_commanded_positions = self.right.get_names_and_cmd_joints()
-        joint_positions = dict(zip(left_names + right_names, left_commanded_positions + right_commanded_positions))
+        # right_names, right_commanded_positions = self.right.get_names_and_cmd_joints()
+        # joint_positions = dict(zip(left_names + right_names, left_commanded_positions + right_commanded_positions))
+        joint_positions = dict(zip(left_names , left_commanded_positions ))
         return joint_positions
 
     def get_joint_pos_dict(self):
         left_names, left_measured_positions = self.left.get_names_and_measured_joints()
-        right_names, right_measured_positions = self.right.get_names_and_measured_joints()
-        joint_positions = dict(zip(left_names + right_names, left_measured_positions + right_measured_positions))
+        # right_names, right_measured_positions = self.right.get_names_and_measured_joints()
+        # joint_positions = dict(zip(left_names + right_names, left_measured_positions + right_measured_positions))
+        joint_positions = dict(zip(left_names , left_measured_positions ))
         return joint_positions
 
     def __init_compute_ik(self):
